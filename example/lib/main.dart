5e4e2b3c3f2fc5a5cec88722ba86914de1daf9ac
Yudi Setiawan <yudisetiawan@nusa.net.id>
2021-06-01 18:09:18 +0700

    Update example
    Update example for default image function

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:image_editor_pro/image_editor_pro.dart';
 import 'package:firexcode/firexcode.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() {
   runApp(MyApp());
@@ -19,14 +20,16 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final controllerDefaultImage = TextEditingController();
+  File _defaultImage;
   File _image;
 
-  Future<void> getimageditor() =>
-      Navigator.push(context, MaterialPageRoute(builder: (context) {
+  Future<void> getimageditor() => Navigator.push(context, MaterialPageRoute(builder: (context) {
         return ImageEditorPro(
           appBarColor: Colors.black87,
           bottomBarColor: Colors.black87,
           pathSave: null,
+          defaultImage: _defaultImage,
         );
       })).then((geteditimage) {
         if (geteditimage != null) {
@@ -42,17 +45,44 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return condition(
             condtion: _image == null,
-            isTrue: 'Open Editor'.text().xRaisedButton(
-              onPressed: () {
-                getimageditor();
-              },
-            ).toCenter(),
-            isFalse:
-                _image == null ? Container() : Image.file(_image).toCenter())
+            isTrue: XColumn(crossAxisAlignment: CrossAxisAlignment.center)
+                .list([
+                  TextField(
+                    controller: controllerDefaultImage,
+                    readOnly: true,
+                    decoration: InputDecoration(
+                      isDense: true,
+                      hintText: 'No default image',
+                    ),
+                  ),
+                  16.0.sizedHeight(),
+                  'Set Default Image'.text().xRaisedButton(
+                    onPressed: () async {
+                      final imageGallery = await ImagePicker().getImage(source: ImageSource.gallery);
+                      if (imageGallery != null) {
+                        _defaultImage = File(imageGallery.path);
+                        setState(() => controllerDefaultImage.text = _defaultImage.path);
+                      }
+                    },
+                  ),
+                  'Open Editor'.text().xRaisedButton(
+                    onPressed: () {
+                      getimageditor();
+                    },
+                  ),
+                ])
+                .xCenter()
+                .xap(value: 16),
+            isFalse: _image == null ? Container() : Image.file(_image).toCenter())
         .xScaffold(
-            appBar: 'Image Editor Pro example'.xTextColorWhite().xAppBar(),
-            floatingActionButton:
-                Icons.add.xIcons().xFloationActiobButton(color: Colors.red));
+      appBar: 'Image Editor Pro example'.xTextColorWhite().xAppBar(),
+      floatingActionButton: Icons.add.xIcons().xFloationActiobButton(
+            color: Colors.red,
+            onTap: () {
+              // TODO: I don't know what I'm doing in here
+            },
+          ),
+    );
   }
 }
 
